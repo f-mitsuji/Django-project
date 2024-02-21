78a9654c518afe6c019d8dca409c8da447443746
三辻史哉 <a5820079@aoyama.ac.jp>
2024-02-21 20:33:29 +0900

    ホームページを実装

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('homepage.urls')),
     path('papers/', include('papers.urls')),
     path('accounts/', include('accounts.urls')),
 ]

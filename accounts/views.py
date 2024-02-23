adf07c539b499fe32d9a3fcedc18eab0c9a99116
三辻史哉 <fumiya.mitsuji@gmail.com>
2024-02-24 02:44:42 +0900

    accountsの名前空間の設定

--- accounts/views.py
+++ accounts/views.py
@@ -6,16 +6,16 @@ from django.views.generic import CreateView
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('accounts:login')
 
 
 class CustomLoginView(LoginView):
     form_class = CustomUserLoginForm
     template_name = 'accounts/login.html'
     redirect_authenticated_user = True
-    next_page = reverse_lazy('paper_list')
+    next_page = reverse_lazy('papers:paper_list')
 
 
 class CustomLogoutView(LogoutView):
-    next_page = reverse_lazy('login')
+    next_page = reverse_lazy('accounts:login')
